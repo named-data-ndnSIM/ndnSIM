42b4924c35bf96d8c68ccd0f256a20e8a3347b93
Ilya Moiseenko <iliamo@ucla.edu>
2011-08-05 20:23:30 -0700

    simple Interest Generator was added

--- model/ndn_contentpacket.cc
+++ model/ndn_contentpacket.cc
@@ -11,7 +11,8 @@ namespace ns3
 {
 namespace NDNabstraction
 {
-    ContentPacket::ContentPacket(const struct ccn_charbuf *Name,const void *data,size_t size):Packet()
+    ContentPacket::ContentPacket(const struct ccn_charbuf *Name,const void *data,size_t size)
+    : Packet()
     {
         ccn_charbuf *output = ccn_charbuf_create();
         int result = ccn_encode_ContentObject(output,Name,data,size);
