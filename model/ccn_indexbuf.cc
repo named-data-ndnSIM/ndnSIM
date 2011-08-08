6b997c549c37254f7e8c32852f0dca74b98945a0
Alexander Afanasyev <alexander.afanasyev@ucla.edu>
2011-08-08 12:55:25 -0700

    Adding emacs header-lines and copyright notices

--- model/ccn_indexbuf.cc
+++ model/ccn_indexbuf.cc
@@ -1,10 +1,21 @@
+/* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
 /*
- *  ccn_indexbuf.cc
- *  Abstraction
+ * Copyright (c) 2011 University of California, Los Angeles
  *
- *  Created by Ilya on 7/29/11.
- *  Copyright 2011 __MyCompanyName__. All rights reserved.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
  *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Ilya Moiseenko <iliamo@cs.ucla.edu>
  */
 
 #include "ccn_indexbuf.h"
