242076251adf43a6682f10609a1bbdb2bd288752
Alexander Afanasyev <alexander.afanasyev@ucla.edu>
2013-07-15 18:27:56 -0700

    examples: ndn-simple-with-pcap example now used CCNb packet format
    The output of the example can be parsed using, for example, ndndump tool
    (https://github.com/named-data/ndndump):
    
       ndndump -o -o ndn-simple-trace.pcap
    
    Refs #1008 (http://redmine.named-data.net/)

--- examples/ndn-simple-with-pcap.cc
+++ examples/ndn-simple-with-pcap.cc
@@ -56,6 +56,8 @@ main (int argc, char *argv[])
   Config::SetDefault ("ns3::PointToPointChannel::Delay", StringValue ("10ms"));
   Config::SetDefault ("ns3::DropTailQueue::MaxPackets", StringValue ("20"));
 
+  Config::SetDefault ("ns3::ndn::Face::WireFormat", StringValue ("1"));
+
   // Creating nodes
   NodeContainer nodes;
   nodes.Create (3);
