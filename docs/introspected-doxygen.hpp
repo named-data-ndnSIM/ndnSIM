eb7d530d41a63b6b3e0119a9f228a415c8064fed
Alexander Afanasyev <alexander.afanasyev@ucla.edu>
2014-12-20 14:35:29 -0800

    bindings: Removing all automatically generated bindings
    This commit keeps template to create empty ndnSIM python module.

---
+++ docs/introspected-doxygen.hpp
@@ -0,0 +1,4 @@
+Usage:
+../../../ build / utils / ns3 - dev - print - introspected - doxygen - debug[options]
+
+  Options : --help : print these options-- output - text : format output as plain text
