spirosmastorakis/ndnSIM-development

build: Fixing of includes

apps/ndn-consumer-zipf-mandelbrot.cpp
@@ -18,12 +18,11 @@
  * Author: Xiaoke Jiang <shock.jiang@gmail.com>
  */
 
-#include "ndn-consumer-zipf-mandelbrot.h"
+#include "ndn-consumer-zipf-mandelbrot.hpp"
 
-#include "ns3/ndn-app-face.h"
-#include "ns3/ndn-interest.h"
-
-#include "ns3/ndnSIM/utils/ndn-fw-hop-count-tag.h"
+#include "model/ndn-app-face.hpp"
+#include "utils/ndn-interest.hpp"
+#include "utils/ndn-fw-hop-count-tag.hpp"
 
 #include <math.h>
 

apps/ndn-producer.cpp
@@ -19,22 +19,22 @@
  *         Alexander Afanasyev <alexander.afanasyev@ucla.edu>
  */
 
+#include "ndn-producer.hpp"
+
 #include "ns3/object.h"
-#include "ndn-producer.h"
 #include "ns3/log.h"
 #include "ns3/string.h"
 #include "ns3/uinteger.h"
 #include "ns3/packet.h"
 #include "ns3/simulator.h"
 
-#include "ns3/ndn-app-face.h"
-#include "ns3/ndnSIM/NFD/daemon/table/fib.hpp"
-#include "ns3/ndn-ns3.hpp"
-#include "ns3/ndn-l3-protocol.h"
-#include "ns3/ndn-fib-helper.h"
-#include "ns3/ndn-fw-hop-count-tag.h"
-#include "ns3/ndn-interest.h"
-#include "ns3/ndn-data.h"
+#include "model/ndn-app-face.hpp"
+#include "model/ndn-ns3.hpp"
+#include "model/ndn-l3-protocol.hpp"
+#include "helper/ndn-fib-helper.hpp"
+#include "utils/ndn-fw-hop-count-tag.hpp"
+#include "utils/ndn-interest.hpp"
+#include "utils/ndn-data.hpp"
 
 #include <boost/ref.hpp>
 #include <boost/lambda/lambda.hpp>
@@ -104,10 +104,6 @@ Producer::StartApplication ()
 
   NS_LOG_DEBUG ("NodeID: " << GetNode ()->GetId ());
 
-  // ::ndn::shared_ptr< ::nfd::fib::Entry> entry =
-  //    GetNode ()->GetObject<L3Protocol> ()->GetForwarder ()->getFib ().insert (m_prefix).first;
-  // entry->addNextHop (m_face->shared_from_this (), 0);
-
   ControlParameters parameters;
   parameters.setName(m_prefix);
   parameters.setFaceId(m_face->getId ());
@@ -116,13 +112,6 @@ Producer::StartApplication ()
   FibHelper fibHelper;
   Ptr<Node> node = GetNode();
   fibHelper.AddNextHop(parameters, node);
-
-  // fibEntry->UpdateStatus (m_face, fib::FaceMetric::NDN_FIB_GREEN);
-
-  // // make face green, so it will be used primarily
-  // StaticCast<fib::FibImpl> (fib)->modify (fibEntry,
-  //                                         ll::bind (&fib::Entry::UpdateStatus,
-  //                                                   ll::_1, m_face, fib::FaceMetric::NDN_FIB_GREEN));
 }
 
 void

utils/topology/annotated-topology-reader.cpp
@@ -19,7 +19,7 @@
  *         Ilya Moiseenko <iliamo@cs.ucla.edu>
  */
 
-#include "annotated-topology-reader.h"
+#include "annotated-topology-reader.hpp"
 
 #include "ns3/nstime.h"
 #include "ns3/log.h"
@@ -38,12 +38,12 @@
 #include "ns3/pointer.h"
 #include "ns3/uinteger.h"
 #include "ns3/ipv4-address.h"
-#include "ns3/ndn-l3-protocol.h"
-#include "ns3/ndn-face.h"
 #include "ns3/random-variable.h"
 #include "ns3/error-model.h"
-
 #include "ns3/constant-position-mobility-model.h"
+
+#include "model/ndn-l3-protocol.hpp"
+#include "model/ndn-face.hpp"
 
 #include <boost/foreach.hpp>
 #include <boost/lexical_cast.hpp>

utils/tracers/ndn-l3-aggregate-tracer.cpp
@@ -18,14 +18,16 @@
  * Author:  Alexander Afanasyev <alexander.afanasyev@ucla.edu>
  */
 
-#include "ndn-l3-aggregate-tracer.h"
+#include "ndn-l3-aggregate-tracer.hpp"
 
 #include "ns3/node.h"
 #include "ns3/packet.h"
 #include "ns3/config.h"
 #include "ns3/callback.h"
-#include "ns3/ndn-app.h"
-#include "ns3/ndn-face.h"
+
+#include "apps/ndn-app.hpp"
+#include "model/ndn-face.hpp"
+
 #include <ndn-cxx/interest.hpp>
 #include <ndn-cxx/data.hpp>
 

named-data-ndnSIM/ndnSIM

apps/ndn-consumer-zipf-mandelbrot.cpp
@@ -18,13 +18,13 @@
  * Author: Xiaoke Jiang <shock.jiang@gmail.com>
  */
 
-#include "ndn-consumer-zipf-mandelbrot.h"
+#include "ndn-consumer-zipf-mandelbrot.hpp"
 
-#include "ns3/ndn-app-face.h"
-#include "ns3/ndn-interest.h"
-#include "ns3/ndn-data.h"
+#include "ns3/ndn-app-face.hpp"
+#include "ns3/ndn-interest.hpp"
+#include "ns3/ndn-data.hpp"
 
-#include "ns3/ndnSIM/utils/ndn-fw-hop-count-tag.h"
+#include "ns3/ndnSIM/utils/ndn-fw-hop-count-tag.hpp"
 
 #include <math.h>
 

apps/ndn-producer.cpp
@@ -19,19 +19,19 @@
  *         Alexander Afanasyev <alexander.afanasyev@ucla.edu>
  */
 
-#include "ndn-producer.h"
+#include "ndn-producer.hpp"
 #include "ns3/log.h"
-#include "ns3/ndn-interest.h"
-#include "ns3/ndn-data.h"
+#include "ns3/ndn-interest.hpp"
+#include "ns3/ndn-data.hpp"
 #include "ns3/string.h"
 #include "ns3/uinteger.h"
 #include "ns3/packet.h"
 #include "ns3/simulator.h"
 
-#include "ns3/ndn-app-face.h"
-#include "ns3/ndn-fib.h"
+#include "ns3/ndn-app-face.hpp"
+#include "ns3/ndn-fib.hpp"
 
-#include "ns3/ndnSIM/utils/ndn-fw-hop-count-tag.h"
+#include "ns3/ndnSIM/utils/ndn-fw-hop-count-tag.hpp"
 
 #include <boost/ref.hpp>
 #include <boost/lambda/lambda.hpp>

plugins/topology/annotated-topology-reader.cpp
@@ -19,7 +19,7 @@
  *         Ilya Moiseenko <iliamo@cs.ucla.edu>
  */
 
-#include "annotated-topology-reader.h"
+#include "annotated-topology-reader.hpp"
 
 #include "ns3/nstime.h"
 #include "ns3/log.h"
@@ -38,8 +38,8 @@
 #include "ns3/pointer.h"
 #include "ns3/uinteger.h"
 #include "ns3/ipv4-address.h"
-#include "ns3/ndn-l3-protocol.h"
-#include "ns3/ndn-face.h"
+#include "ns3/ndn-l3-protocol.hpp"
+#include "ns3/ndn-face.hpp"
 #include "ns3/random-variable.h"
 #include "ns3/error-model.h"
 

utils/tracers/ndn-l3-aggregate-tracer.cpp
@@ -18,17 +18,17 @@
  * Author:  Alexander Afanasyev <alexander.afanasyev@ucla.edu>
  */
 
-#include "ndn-l3-aggregate-tracer.h"
+#include "ndn-l3-aggregate-tracer.hpp"
 
 #include "ns3/node.h"
 #include "ns3/packet.h"
 #include "ns3/config.h"
 #include "ns3/callback.h"
-#include "ns3/ndn-app.h"
-#include "ns3/ndn-face.h"
-#include "ns3/ndn-interest.h"
-#include "ns3/ndn-data.h"
-#include "ns3/ndn-pit-entry.h"
+#include "ns3/ndn-app.hpp"
+#include "ns3/ndn-face.hpp"
+#include "ns3/ndn-interest.hpp"
+#include "ns3/ndn-data.hpp"
+#include "ns3/ndn-pit-entry.hpp"
 
 #include "ns3/simulator.h"
 #include "ns3/node-list.h"