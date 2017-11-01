00f3839cb2c85540f0d628a13e79ee3977402534
Eric Newberry <enewberry@cs.arizona.edu>
2017-11-01 15:21:35 -0700

    build: allow Jenkins to specify number of waf build jobs
    Change-Id: Idb58cdf572ab6a240300c9febbb2cfe9638e8233

--- .jenkins.d/10-build.sh
+++ .jenkins.d/10-build.sh
@@ -14,11 +14,11 @@ sudo rm -Rf build/ .waf-1* .waf3-1*
 find . -name '*.pyc' | sudo xargs rm -f
 
 # Configure/build in debug mode
-./waf -j1 configure -d debug --enable-modules=ndnSIM --enable-examples --enable-tests $EXTRA_FLAGS
-./waf -j1 build
+./waf -j${WAF_JOBS:-1} configure -d debug --enable-modules=ndnSIM --enable-examples --enable-tests $EXTRA_FLAGS
+./waf -j${WAF_JOBS:-1}  build
 
 # Install
-sudo ./waf -j1 install
+sudo ./waf -j${WAF_JOBS:-1} install
 
 if has Linux $NODE_LABELS; then
     sudo ldconfig
